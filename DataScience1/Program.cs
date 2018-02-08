7e62eaeba27e161e2cd4cff2aa989185c782c16a
Andy Bhadai <abhadai@gmail.com>
2018-02-08 14:23:51 +0100

    Add project files.

---
+++ DataScience1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataScience1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
