c253d3f45503a1a0f372460ad82f5a291e04358d
Andy Bhadai <abhadai@gmail.com>
2018-02-13 11:43:46 +0100

    Added simple Dictionary implementation

--- DataScience1/Program.cs
+++ DataScience1/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DataScience1
 {
+    public class Dictionary<TKey, TValue>
+    {
+        public List<KeyValuePair<TKey, TValue>> List = new List<KeyValuePair<TKey, TValue>>();
+
+        public void Add(TKey key, TValue value)
+        {
+            KeyValuePair<TKey, TValue> kvp = new KeyValuePair<TKey, TValue>(key, value);
+            this.List.Add(kvp);
+        }
+    }
+
+    public class KeyValuePair<TKey, TValue>
+    {
+        public TKey Key;
+        public TValue Value;
+
+        public KeyValuePair(TKey key, TValue value)
+        {
+            this.Key = key;
+            this.Value = value;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<string, string> dictionary = new Dictionary<string, string>();
+            dictionary.Add("Mark", "Romy");
+            dictionary.Add("Mark", "Jason");
+            dictionary.Add("Mark", "Documentation Dilan");
+            dictionary.Add("Mark", "Mark");
+            Console.ReadLine();
         }
     }
 }
